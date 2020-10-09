dda00f36e71c46ea6505f96a818bac4c9db19fe8
ani150101 <singhaniket150101@gmail.com>
2020-10-10 03:38:03 +0530

    Added dmEmbed function

--- embed.js
+++ embed.js
@@ -23,8 +23,24 @@ const normalEmbed = (message, description, color, title, titleUrl) => {
     if(titleUrl) embed.setURL(titleurl);
     message.channel.send(embed);
 }
+async function dmEmbed (message, member, description, title, titleUrl, authorBool, thumbnailUrl, fields, color, imageUrl) {
+    let embed = new discord.MessageEmbed();
+    embed.setFooter('Developed by Robot & Coco', message.guild.iconURL());
+    embed.setTimestamp();
+    if(authorBool) embed.setAuthor(message.author.tag, message.author.displayAvatarURL());
+    if(description) embed.setDescription(description);
+    if(title) embed.setTitle(title);
+    if(titleUrl) embed.setURL(titleurl);
+    if(thumbnailUrl) embed.setThumbnail(thumbnailUrl);
+    if(fields) embed.addFields(fields);
+    if(color) {embed.setColor(color);}
+    else {embed.setColor("#b33030");}
+    if(imageUrl) embed.setImage(imageUrl);
+    member.send(embed);
+};
 
 module.exports = {
     richEmbed,
     normalEmbed,
+    dmEmbed,
 };
