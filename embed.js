69767f36342add90cbd973e83de55278fb6825c2
ani150101 <singhaniket150101@gmail.com>
2020-10-09 16:16:12 +0530

    Seperate file for embed functions.

---
+++ embed.js
@@ -0,0 +1,30 @@
+const discord = require('discord.js');
+const richEmbed = (message, description, title, titleUrl, authorBool, thumbnailUrl, fields, color, imageUrl) => {
+    let embed = new discord.MessageEmbed();
+    embed.setFooter('Developed by Robot & Coco', message.guild.iconURL());
+    embed.setTimestamp();
+    if(authorBool) embed.setAuthor(message.author.tag, message.author.displayAvatarURL());
+    if(description) embed.setDescription(description);
+    if(title) embed.setTitle(title);
+    if(titleUrl) embed.setURL(titleurl);
+    if(thumbnailUrl) embed.setThumbnail(thumbnailUrl);
+    if(fields) embed.addFields(fields);
+    if(color) {embed.setColor(color);}
+    else {embed.setColor("#b33030");}
+    if(imageUrl) embed.setImage(imageUrl);
+    message.channel.send(embed);
+};
+const normalEmbed = (message, description, color, title, titleUrl) => {
+    let embed = new discord.MessageEmbed();
+    if(description) embed.setDescription(description);
+    if(color) {embed.setColor(color);}
+    else {embed.setColor("#b33030");}
+    if(title) embed.setTitle(title);
+    if(titleUrl) embed.setURL(titleurl);
+    message.channel.send(embed);
+}
+
+module.exports = {
+    richEmbed,
+    normalEmbed,
+};
